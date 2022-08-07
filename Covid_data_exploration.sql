6ea368898769879016adc161aa70227424b4055e
RahmanEmon <48353914+RahmanEmon@users.noreply.github.com>
2022-08-07 23:00:31 +0600

    death percentage

---
+++ Covid_data_exploration.sql
@@ -0,0 +1,8 @@
+-- Data visualization of total cases vs total deaths against the population of a country.
+
+SELECT location, date, population, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
+	FROM coviddeaths
+	ORDER BY 1,2,6;
+
+
+
